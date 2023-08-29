549df2cab1a70b5391ab00d0fd2a67352372a023
Vladislav <dva.20002012@yandex.ru>
2023-08-29 11:34:37 +0300

    Добавил свои файлы

---
+++ mainwindow.cpp
@@ -0,0 +1,71 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+
+    textEdit = new QTextEdit(this);
+
+    setCentralWidget(textEdit);
+
+    createMenu();
+
+    setWindowTitle("Clipboard");
+
+}
+
+void MainWindow::copyToClipboard() {
+
+    clipboard = QApplication::clipboard();
+
+    QString selectedText = textEdit->textCursor().selectedText();
+
+    if (selectedText.isEmpty()) {
+
+        QMessageBox::warning(this, "Clipboard", "No text selected!");
+
+        return;
+
+    }
+
+    clipboard->setText(selectedText);
+
+}
+
+void MainWindow::pasteFromClipboard() {
+
+    clipboard = QApplication::clipboard();
+
+    QString clipboardText = clipboard->text();
+
+    if (clipboardText.isEmpty()) {
+
+        QMessageBox::warning(this, "Clipboard", "The buffer is empty!");
+
+        return;
+
+    }
+
+    textEdit->insertPlainText(clipboardText);
+
+}
+
+void MainWindow::createMenu() {
+
+    QMenu *editMenu = menuBar()->addMenu(tr("&Edit"));
+
+    copyAction = new QAction(tr("Copy"), this);
+
+    copyAction->setShortcuts(QKeySequence::Copy);
+
+    connect(copyAction, &QAction::triggered, this, &MainWindow::copyToClipboard);
+
+    editMenu->addAction(copyAction);
+
+    pasteAction = new QAction(tr("Paste"), this);
+
+    pasteAction->setShortcuts(QKeySequence::Paste);
+
+    connect(pasteAction, &QAction::triggered, this, &MainWindow::pasteFromClipboard);
+
+    editMenu->addAction(pasteAction);
+
+}
