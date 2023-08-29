b4054675fc28c4c8823851cfddef2a9259cb1a7a
Vladislav <dva.20002012@yandex.ru>
2023-08-29 12:47:30 +0300

    Востановил

--- mainwindow.h
+++ mainwindow.h
@@ -1,39 +1,21 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+
 #include <QMainWindow>
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QMainWindow>
-#include <QtWidgets/QTextEdit>
-#include <QtGui/QClipboard>
-#include <QtWidgets/QMessageBox>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QMenu>
 
-class MainWindow : public QMainWindow {
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
-
-    explicit MainWindow(QWidget *parent = nullptr);
-
-private slots:
-
-    void copyToClipboard();
-
-    void pasteFromClipboard();
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
 
 private:
-
-    QTextEdit *textEdit;
-
-    QAction *copyAction;
-
-    QAction *pasteAction;
-
-    QClipboard *clipboard;
-
-    void createMenu();
-
+    Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
