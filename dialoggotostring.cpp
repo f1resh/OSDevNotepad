c97081aca9f193ef0428872739e27815244e42c6
RozaBlack <88147903+RozaBlack@users.noreply.github.com>
2023-09-26 14:00:59 +0300

    Update dialoggotostring.cpp
    Текст переведен на русский язык,
    добавлено нижнее ограничение равное 1 для QSpinBox,
    добавлен указатель на QMdiArea при инициализации класса,
    изменено объявление библиотек

--- dialoggotostring.cpp
+++ dialoggotostring.cpp
@@ -1,18 +1,31 @@
 #include "dialoggotostring.h"
+#include <QPushButton>
+#include <QGridLayout>
+#include <QSpinBox>
+#include <QLabel>
+#include <QMdiArea>
 
-dialogGoToString::dialogGoToString(QWidget *parent)
+
+DialogGoToString::DialogGoToString(QMdiArea* mainArea, QWidget *parent)
     : QDialog(parent, Qt::WindowTitleHint | Qt::WindowSystemMenuHint)
 {
     m_lineNumber = new QSpinBox;
+    m_lineNumber->setMinimum(1);
 
-    strNumber = new QLabel("String number:");
+    strNumber = new QLabel(this);
     strNumber->setBuddy(m_lineNumber);
 
-    goTo   = new QPushButton("&Go");
-    cancel = new QPushButton("&Cancel");
+    goTo   = new QPushButton(this);
+    cancel = new QPushButton(this);
 
     connect(goTo, SIGNAL(clicked()), SLOT(accept()));
     connect(cancel, SIGNAL(clicked()), SLOT(reject()));
+
+
+    goTo->setText(tr("Перейти"));
+    cancel->setText(tr("Отмена"));
+    strNumber->setText(tr("№ строки:"));
+
 
     //Layout setup
     QGridLayout* ptopLayout = new QGridLayout;
@@ -21,11 +34,13 @@ dialogGoToString::dialogGoToString(QWidget *parent)
     ptopLayout->addWidget(goTo, 10, 0);
     ptopLayout->addWidget(cancel, 10, 1);
     setLayout(ptopLayout);
+
+
 }
 
-dialogGoToString::~dialogGoToString() {}
+DialogGoToString::~DialogGoToString() {}
 
-int dialogGoToString::lineNumber() const
+int DialogGoToString::lineNumber() const
 {
     return m_lineNumber->value();
 }
