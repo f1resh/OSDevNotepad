ab8fc6205c4981067ef47f9cc4103cd3be8b73ab
RozaBlack <88147903+RozaBlack@users.noreply.github.com>
2023-10-01 23:17:34 +0300

    Update dialoggotostring.h
    Добавлены методы для определения количества строк в документе и перехода к строке с указанным номером

--- dialoggotostring.h
+++ dialoggotostring.h
@@ -1,29 +1,34 @@
-#ifndef DIALOGGOTOSTRING_H
-#define DIALOGGOTOSTRING_H
-
 #include <QDialog>
 #include <QWidget>
 
 
-    class QPushButton;
-    class QGridLayout;
-    class QSpinBox;
-    class QLabel;
-    class QMdiArea;
+class QPushButton;
+class QGridLayout;
+class QSpinBox;
+class QLabel;
+class QMdiArea;
+class QTextEdit;
 
+// Класс DialogGoToString для перехода к строке с указанным номером
 
 class DialogGoToString : public QDialog
 {
 public:
-    DialogGoToString(QMdiArea* mainArea, QWidget *parent = nullptr);
-    ~DialogGoToString();
-    int lineNumber() const;
+    DialogGoToString(QMdiArea* area, QWidget *parent = nullptr); //Конструктор класса DialogGoToString
+    ~DialogGoToString(); // Деструктор класса DialogGoToString
+    int lineNumber() const; // Метод для получения номера строки к которой нужно перейти
 
 private:
-    QPushButton* goTo;
-    QPushButton* cancel;
-    QSpinBox* m_lineNumber;
-    QLabel* strNumber;
-};
+    QPushButton* goTo; // Кнопка для перехода к строке с указанным номером
+    QPushButton* cancel; // Кнопка для закрытиф формы
+    QSpinBox* m_lineNumber; // Поле для ввода номера строки
+    QLabel* strNumber; // Подпись поля для ввода номера строки
+    QMdiArea* mainArea; // Указатель на главное окно QMdiArea
 
-#endif // DIALOGGOTOSTRING_H
+    QTextEdit* getCurrentTextEdit(QMdiArea* mainArea = nullptr); // Метод для получения указателя на текущий QTextEdit в QMdiArea
+    int getStringsNumbers(QTextEdit* currentTextEdit = nullptr); // Метод для получения количества строк в докуметне
+    void init(); // Метод инициализации класса
+
+private slots:
+    void moveCursorToString(); // Слот для перемещения курсора в начало строки с указанным номером
+};
